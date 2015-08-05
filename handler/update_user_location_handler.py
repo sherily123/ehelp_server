b7228dbce09d41c32795a67e706dd2e154038aee
sherily123 <sherily123@163.com>
2015-08-05 14:26:26 +0800

    update user current location

---
+++ handler/update_user_location_handler.py
@@ -0,0 +1,27 @@
+#!/usr/python
+
+from tornado.web import RequestHandler
+from tornado.escape import json_encode
+
+
+from utils import utils
+from utils import KEY
+from utils import STATUS
+#from utils import baidulbs
+from database import db
+
+
+class Update_User_Location_Handler(RequestHandler):
+  def post(self):
+    params = utils.decode_params(self.request)
+
+    resp = {}
+    result = db.update_location(params)
+    #baiduResult = baidulbs.update_location(params, KEY.USER)
+    if result:
+      resp[KEY.STATUS] = STATUS.OK
+    else:
+      resp[KEY.STATUS] = STATUS.ERROR
+
+    self.write(json_encode(resp))
+
